4fac1a187797d684dff6fe29bdcd9521f9f67021
jimsey <the.james.burton@gmail.com>
2019-04-30 19:14:56 +0100

    safe navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+interface Child {
+  name: string;
+  age: number;
+}
+
 interface Passenger {
   id: number;
   fullname: string;
   checkedIn: boolean;
   checkInDate: number | null;
+  children: Child[] | null;
 }
 
 @Component({
@@ -26,6 +32,10 @@ interface Passenger {
           <!-- different to course: yMMMMd replaced with longDate -->
           {{ passenger.checkInDate ? (passenger.checkInDate | date: 'longDate' | uppercase ) : 'Not checked in' }}
         </div>
+        <div class="children">
+          <!-- this is the safe navigation -->
+          Children: {{ passenger.children?.length || 0 }}
+        </div>
       </li>
     </ul>
 </div>
@@ -37,26 +47,33 @@ export class AppComponent {
     id: 1,
     fullname: 'John',
     checkedIn: true,
-    checkInDate: 1490742000000
+    checkInDate: 1490742000000,
+    children: null
   }, {
     id: 2,
     fullname: 'Julie',
     checkedIn: false,
-    checkInDate: null
+    checkInDate: null,
+    children: [{ name: 'Alan', age: 12 },{ name: 'Alex', age: 7 }]
   }, {
     id: 3,
     fullname: 'James',
     checkedIn: true,
-    checkInDate: 1491606000000
+    checkInDate: 1491606000000,
+    children: null
+
   }, {
     id: 4,
     fullname: 'Joseph',
     checkedIn: true,
-    checkInDate: 1488412800000
+    checkInDate: 1488412800000,
+    children: [{ name: 'Amy', age: 1 }]
   }, {
     id: 5,
     fullname: 'Josie',
     checkedIn: false,
-    checkInDate: null
+    checkInDate: null,
+    children: null
+
 }];
 }
