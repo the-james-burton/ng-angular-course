df7a5fa227d7ca82821d7338e10d69b4af9bb838
jimsey <the.james.burton@gmail.com>
2019-05-01 07:12:05 +0100

    input data

--- src/app/passenger-dashboard/components/passenger-count/passenger.count.component.ts
+++ src/app/passenger-dashboard/components/passenger-count/passenger.count.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { Passenger } from '../../models/passenger.interface';
 
 @Component({
   selector: 'passenger-count',
   template: `
     <div>
-      Count component
+      <h3>Airline Passengers2</h3>
+      <div>
+        Total checked in: {{ checkedInCount() }} / {{ items.length }}
+      </div>
     </div>
   `
 })
 export class PassengerCountComponent {
-  constructor() {}
+  @Input()
+  items: Passenger[];
+  constructor() {};
+  checkedInCount(): number {
+    if (!this.items) return;
+    return this.items.filter((passenger: Passenger) => passenger.checkedIn).length;
+  }
 }
