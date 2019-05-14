887c8d0329ba32624e321b90d88753bef0db0a51
jimsey <the.james.burton@gmail.com>
2019-05-14 06:55:09 +0100

    wilcard not found

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { PassengerDashboardModule } from './passenger-dashboard/passenger-dashboard.module';
 
 import { HomeComponent } from './home.component';
+import { NotFoundComponent } from './not-found.component';
+
 import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, pathMatch: 'full' }
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent, pathMatch: 'full' }
 ];
 
 @NgModule({
@@ -19,7 +22,7 @@ const routes: Routes = [
     PassengerDashboardModule,
     RouterModule.forRoot(routes)
   ],
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, NotFoundComponent],
   bootstrap: [AppComponent],
   providers: []
 })
---
+++ src/app/not-found.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'not-found',
+  template: `
+    <div>
+      Not Found
+    </div>
+  `
+
+})
+export class NotFoundComponent {}
