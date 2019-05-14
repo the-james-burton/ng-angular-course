63e2452b60c29ece977c16c90dd5559626cdd000
jimsey <the.james.burton@gmail.com>
2019-05-14 06:35:16 +0100

    ngsubmit stateless output

--- src/app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ src/app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 import { Passenger } from '../../models/passenger.interface';
 import { Baggage } from '../../models/baggage.interface';
@@ -7,7 +7,7 @@ import { Baggage } from '../../models/baggage.interface';
   selector: 'passenger-form',
   styleUrls: ['passenger-form.component.scss'],
   template: `
-    <form #form="ngForm" novalidate>
+    <form (ngSubmit)="handleSubmit(form.value, form.valid)" #form="ngForm" novalidate>
       {{ detail | json }}
       <div>
         Passenger name:
@@ -82,8 +82,12 @@ import { Baggage } from '../../models/baggage.interface';
   `
 })
 export class PassengerFormComponent {
+
   @Input()
   detail: Passenger;
+
+  @Output()
+  update: EventEmitter<Passenger> = new EventEmitter();
 
   baggage: Baggage[] = [
     {
@@ -108,6 +112,12 @@ export class PassengerFormComponent {
     if (checkedIn) {
       // in real world, this would likely be done on the server...
       this.detail.checkInDate = Date.now();
+    }
+  }
+
+  handleSubmit(passenger: Passenger, isValid: boolean) {
+    if (isValid) {
+      this.update.emit(passenger);
     }
   }
 }
--- src/app/passenger-dashboard/containers/passenger-viewer/passenger-viewer.component.ts
+++ src/app/passenger-dashboard/containers/passenger-viewer/passenger-viewer.component.ts
@@ -9,8 +9,7 @@ import { Passenger } from '../../models/passenger.interface';
   styleUrls: ['passenger-viewer.component.scss'],
   template: `
     <div>
-      <passenger-form
-        [detail]="passenger">
+      <passenger-form [detail]="passenger" (update)="onUpdatePassenger($event)">
       </passenger-form>
     </div>
   `
@@ -22,5 +21,14 @@ export class PassengerViewerComponent implements OnInit {
     this.passengerService
       .getPassenger(1)
       .subscribe((data: Passenger) => (this.passenger = data));
+  }
+
+  onUpdatePassenger(event: Passenger) {
+    console.log(event);
+    this.passengerService
+      .updatePassenger(event)
+      .subscribe((data: Passenger) => {
+        this.passenger = Object.assign({}, this.passenger, event);
+      });
   }
 }
